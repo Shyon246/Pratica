8244058b2be27bb1575fb4e73c691874f4466a49
Milton Shyon de Lima Gurgel <milton.shyon@academico.ifrn.edu.br>
2023-11-20 10:38:16 -0300

    primeiro teste de como criar um controlador com a pitica ao lado me monitorando constantemente

---
+++ Teste01/Teste01/Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Teste01.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        // Defina um DbSet para cada modelo (entidade) que deseja armazenar no banco de dados
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Configurar o provedor de banco de dados e a string de conexão
+                optionsBuilder.UseSqlServer("sua_string_de_conexao");
+            }
+        }
+}
